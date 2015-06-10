a7f1a7563d5483dff44ab1896ff5dcbb3cef627e
mcdeeda <emcde94@gmail.com>
2015-06-10 07:53:14 -0400

    heart update
    added collision detection and particle effects for hearts. gave hitbox
    to hearts and dog. set dog as trigger. gave dog basic movement. set
    stage to move forward towards player.

---
+++ John Legend 3D/Assets/scripts/dog_move.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dog_move : MonoBehaviour {
+
+	public float dog_pos = 1.0f;
+	public float dog_left = 7.6f;
+	public float dog_center = 6.4f;
+	public float dog_right = 5.2f;
+	public float dog_shift = 1.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.W)){
+			if(dog_pos != 0.0){
+				dog_pos -= 1;
+			}
+			if(dog_pos == 0.0f || dog_pos == 1.0f) {
+				gameObject.transform.Translate(dog_shift,0,0);
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Q)){
+			if(dog_pos !=2.0){
+				dog_pos += 1;
+			}
+			if(dog_pos == 1.0f || dog_pos == 2.0f) {
+				gameObject.transform.Translate(-dog_shift,0,0);
+			}
+		}
+	}
+}
---
+++ John Legend 3D/Assets/scripts/heart_trigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class heart_trigger : MonoBehaviour {
+	
+	private ParticleSystem ps;
+	
+	// Use this for initialization
+	void Start () {
+		ps = gameObject.GetComponent<ParticleSystem>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerEnter() {
+		ps.Play();
+		Debug.Log("particles");
+		Destroy (ps,0.6f);
+		foreach(Transform child in transform){
+			Destroy(child.gameObject);
+		}
+	}
+}
